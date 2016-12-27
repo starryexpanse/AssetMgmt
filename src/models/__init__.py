355f0dcbb420eed409d15e0519d1f8b31741bf38
Philip Peterson <philip@amplitude.com>
2016-12-27 12:11:39 -0600

    Models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -19,31 +19,19 @@ Base.query = db_session.query_property()
 tags_assets = Table('tags_assets', Base.metadata,
     Column('tag_id', Integer, ForeignKey('tags.id')),
     Column('asset_id', Integer, ForeignKey('assets.id')),
-    UniqueConstraint('tag_id', 'asset_id', name='uix_1')
+    UniqueConstraint('tag_id', 'asset_id', name = 'uix_1')
 )
 
 class ManagementStatus(enum.Enum):
     APPROVED = 1
     REJECTED = 2
 
-class Upload(Base):
-    __tablename__ = 'uploads'
-    id = Column(Integer, primary_key = True, nullable = False)
-    asset_id = Column(Integer, ForeignKey('assets.id'), primary_key = True, nullable = False)
-    user_id = Column(Integer, ForeignKey('users.id'), primary_key = True, nullable = False)
-    title = Column(String(255), nullable = False)
-    file_extension = Column(String(255), nullable = False)
-    thumbnail = LargeBinary(length = 1024 * 1024 * 2)
-    management_status = Column(Integer, nullable = True)
-    management_comment = Column(String(255), nullable = False)
-
-
 class Asset(Base):
     __tablename__ = 'assets'
     id = Column(Integer, primary_key = True, nullable = False)
     name = Column(String(255), unique = True, nullable = False)
-    tags = relationship('Tag', secondary=tags_assets,
-        backref=backref('assets', lazy='dynamic'))
+    tags = relationship('Tag', secondary = tags_assets,
+        backref = backref('assets', lazy = 'dynamic'))
 
 class User(Base):
     __tablename__ = 'users'
@@ -53,7 +41,26 @@ class User(Base):
     phone = Column(String(255), unique = False, nullable = True)
     gchat_handle = Column(String(255), unique = False, nullable = True)
 
+class Upload(Base):
+    __tablename__ = 'uploads'
+    id = Column(Integer, primary_key = True, unique = True, nullable = False)
+    asset = Column(Integer, ForeignKey('assets.id'), name = 'asset_id', nullable = False)
+    user = Column(Integer, ForeignKey('users.id'), name = 'user_id', nullable = False)
+    title = Column(String(255), nullable = False)
+    file_extension = Column(String(255), nullable = False)
+    thumbnail = LargeBinary(length = 1024 * 1024 * 2)
+    management_status = Column(Integer, nullable = True)
+    management_comment = Column(String(255), nullable = False)
+
 class Tag(Base):
     __tablename__ = 'tags'
     id = Column(Integer, primary_key = True, nullable = False)
     name = Column(String(255), unique = True, nullable = False)
+
+class Rating(Base):
+    __tablename__ = 'ratings'
+    id = Column(Integer, primary_key = True, nullable = False)
+    upload = Column(Integer, ForeignKey('uploads.id'), name = 'upload_id', nullable = False)
+    author = Column(Integer, ForeignKey('users.id'), name = 'author_id', nullable = False)
+    rating = Column(Integer, nullable = True)
+    text = Column(String(1024 * 8), unique = False, nullable = True)
